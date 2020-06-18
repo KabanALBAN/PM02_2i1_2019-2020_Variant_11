2b5c6605e5b580f8eddd84623f2f4070d8e6b6db
KabanALBAN <56335353+KabanALBAN@users.noreply.github.com>
2020-06-18 11:48:36 +0300

    Add files via upload

---
+++ Выполненное задание/main.cs
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() 
+  {
+   int main()
+{
+    cout << "Программа вычисляющая объем цилинда (конуса). Следуйте инструкциям ниже."<<endl;
+    setprecision(5); acos(-1.0);
+    double pi = acos(-1.0), Rosn, H;
+    cout << "Введите, пожалуйста, радиус (см) "; cin >> Rosn;
+    double Sosn = pi*(Rosn*Rosn);
+    cout << "Введите, пожалуйста, высоту (см) "; cin >> H;
+    double V = ( (1/3 + 1%3) * Sosn * H);
+    cout << "Pi " << pi << " Rosn " << Rosn << " H " << H << " Sosn " << Sosn << " V " << V << endl;
+    cout << "Объем цилиндра цилиндра: " << V << endl;
+}  
+  }
+}
